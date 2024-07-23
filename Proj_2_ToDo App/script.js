0ec46c08b38bd569565914f358301a175f3abcff
Ayazsarmad <m.ayazsarmad@gmail.com>
2024-07-23 01:57:16 -0700

    completed

--- Proj_2_ToDo App/script.js
+++ Proj_2_ToDo App/script.js
@@ -8,6 +8,33 @@ function addtask() {
     let li = document.createElement("li");
     li.innerHTML = inputval.value;
     listCont.appendChild(li);
+    let dlt = document.createElement("span");
+    dlt.innerHTML = "\u00d7";
+    li.appendChild(dlt);
   }
   inputval.value = "";
+  saveItem();
 }
+
+listCont.addEventListener(
+  "click",
+  function (e) {
+    if (e.target.tagName === "LI") {
+      e.target.classList.toggle("checked");
+      saveItem();
+    } else if (e.target.tagName === "SPAN") {
+      e.target.parentElement.remove();
+      saveItem();
+    }
+  },
+  false
+);
+
+function saveItem() {
+  localStorage.setItem("data", listCont.innerHTML);
+}
+
+function displayTask() {
+  listCont.innerHTML = localStorage.getItem("data");
+}
+displayTask();
